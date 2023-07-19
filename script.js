b59fd33952946de754d02b5f9d739230bfa3fd68
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-07-19 15:37:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-//complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	get getName(){
+		return this.name;
+	}
+	set setAge(age){
+		this.age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.getName} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+        console.log(`${this.getName} is teaching`);
+    }
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
+
